e5750a2ed5c99eee0051139ba254d763bd366051
chooblarin <choo.bla.rin@gmail.com>
2017-01-06 23:07:43 +0900

    Play with Perlin noise

--- play/sketch.js
+++ play/sketch.js
@@ -1,32 +1,79 @@
+var gif;
+var c;
+var recording = false;
+
 var t = 0;
 
 function setup() {
-  createCanvas(400, 400);
+  c = createCanvas(240, 240);
+  setupGif();
 }
 
 function draw() {
   background(0);
   noStroke();
-  fill(255);
-  translate(width / 2, height / 2);
-  beginShape();
-  for (var i = 0; i <= 540; i++) {
-    var angle = radians(i);
-    var radius = 200;
-    curveVertex(radius * cos(angle), radius * sin(angle));
 
-    radius = 90 * noise(i * 0.01, t * 0.005);
-    var x = radius * cos(angle);
-    var y = radius * sin(angle);
+  push();
+  translate(width / 2, height / 2);
+  drawBlob(0, 1, 100);
+  drawBlob(100, 2, 95);
+  drawBlob(200, 3, 90);
+  drawBlob(300, 1, 85);
+  drawBlob(400, 2, 80);
+  drawBlob(500, 3, 100);
+  drawBlob(600, 1, 95);
+  drawBlob(700, 2, 90);
+  drawBlob(800, 3, 85);
+  drawBlob(900, 1, 80);
+
+  fill(255);
+  ellipse(0, 0, 140);
+  pop();
+
+  t += 2;
+
+  if (recording && frameCount % 2 == 0) {
+    gif.addFrame(c.elt, { delay: 1, copy: true });
+  }
+}
+
+function drawBlob(offset, start, radius) {
+  beginShape();
+  for (let i = 0; i < 360; i += 20) {
+    let n = noise(i * 0.01 + offset, t * 0.01 + offset);
+    let r = radius + map(n, 0, 1, -15, 15);
+
+    fill(255, 20);
+    let angle = radians(i);
+    let phase = -(PI / 2) * start;
+    let x = r * cos(angle + phase);
+    let y = r * sin(angle + phase);
     curveVertex(x, y);
   }
   endShape(CLOSE);
-
-  t += 1;
 }
 
 function keyPressed() {
   if (keyCode == 32) {
     saveFrames("", "png", 3, 30);
   }
+}
+
+function mousePressed() {
+  recording = !recording;
+  if (!recording) {
+    gif.render();
+  }
+}
+
+function setupGif() {
+  gif = new GIF({
+    workers: 2,
+    quality: 40
+  });
+
+  gif.on('finished', function (blob) {
+    window.open(URL.createObjectURL(blob));
+    setupGif();
+  });
 }
